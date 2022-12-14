8649c2c4cff65927d094fb27bf67db65b683e981
ashelake <ashelake88@gmail.com>
2022-12-15 00:47:45 +0530

    routes

--- FrontEnd-Part/user-panel-frontend/src/index.js
+++ FrontEnd-Part/user-panel-frontend/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-  <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </BrowserRouter>
 );
 
--- FrontEnd-Part/user-panel-frontend/src/routes/route.jsx
+++ FrontEnd-Part/user-panel-frontend/src/routes/route.jsx
@@ -1,18 +1,27 @@
 import React from "react";
-import {Route ,Routes} from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import { CategoryPage } from "../Pages/Category.pages";
 import { SingleProductPage } from "../Pages/SignleProduct.pages";
 import { SubCategoryPage } from "../Pages/SubCategory.page";
+import Homepage from "../Pages/Homepage";
 
-function Router(){
-
-    return <>
-        <Routes>
-            <Route path="/category/subcategory/:id" element={<SingleProductPage/>}></Route>
-            <Route path="/:category" element={<CategoryPage/>}></Route>
-            <Route path="/category/:subcategory" element={<SubCategoryPage/>}></Route>
-        </Routes>
+function Router() {
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/category/subcategory/:id"
+          element={<SingleProductPage />}
+        ></Route>
+        <Route path="/:category" element={<CategoryPage />}></Route>
+        <Route
+          path="/category/:subcategory"
+          element={<SubCategoryPage />}
+        ></Route>
+        <Route path="/" element={<Homepage />}></Route>
+      </Routes>
     </>
+  );
 }
 
-export {Router};
+export { Router };
