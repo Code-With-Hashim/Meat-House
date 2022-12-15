18b8cb658ba767f1e86ba5ab0a4469f15b989298
Ajit Shelake <39041032+ashelake@users.noreply.github.com>
2022-12-15 23:25:37 +0530

    Update route.jsx

--- FrontEnd-Part/user-panel-frontend/src/routes/route.jsx
+++ FrontEnd-Part/user-panel-frontend/src/routes/route.jsx
@@ -5,19 +5,24 @@ import { SingleProductPage } from "../Pages/SignleProduct.pages";
 import { SubCategoryPage } from "../Pages/SubCategory.page";
 import Homepage from "../Pages/Homepage";
 
-function Router(){
-
-    return <>
-        <Routes>
-            <Route path="/" element={<Homepage/>} ></Route>
-            <Route path="/category/subcategory/:id" element={<SingleProductPage/>}></Route>
-            <Route path="/:category" element={<CategoryPage/>}></Route>
-            <Route path="/category/:subcategory" element={<SubCategoryPage/>}></Route>
-            <Route path="/" element={<Homepage />}></Route>
-        </Routes>
-
+function Router() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route
+          path="/category/subcategory/:id"
+          element={<SingleProductPage />}
+        ></Route>
+        <Route path="/:category" element={<CategoryPage />}></Route>
+        <Route
+          path="/category/:subcategory"
+          element={<SubCategoryPage />}
+        ></Route>
+        <Route path="/" element={<Homepage />}></Route>
+      </Routes>
     </>
- 
+  );
 }
 
 export { Router };
