f60aa2551bef3f59b7211fc892d91f861e49de2f
sayeedanwar648 <sayeedanwar648@gmail.com>
2022-12-15 22:39:49 +0530

    cart page done

--- FrontEnd-Part/user-panel-frontend/src/index.js
+++ FrontEnd-Part/user-panel-frontend/src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+<ChakraProvider>
+<BrowserRouter>
   <App />
   </BrowserRouter>
+</ChakraProvider>
+   
+  
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
