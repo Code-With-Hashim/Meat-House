f878ebbaa6c6e108496841b5ef4ddfd29f0103e9
ashelake <ashelake88@gmail.com>
2022-12-15 15:20:16 +0530

    category Dropdown

--- FrontEnd-Part/user-panel-frontend/src/components/Navbar.jsx
+++ FrontEnd-Part/user-panel-frontend/src/components/Navbar.jsx
@@ -20,6 +20,54 @@ import {
   Hide,
   Show,
 } from "@chakra-ui/react";
+import Category from "./Category.jsx";
+
+let cat = [
+  {
+    img: "https://dao54xqhg9jfa.cloudfront.net/OMS-Category/34466dbd-a515-edd1-3e99-05000f217cb6/original/Chicken_(2).png",
+    title: "Chicken",
+  },
+  {
+    img: "https://dao54xqhg9jfa.cloudfront.net/OMS-Category/caac432f-545f-f03f-ce10-3b911916da70/original/FIsh_(1).png",
+    title: "Fish & Seafood",
+  },
+  {
+    img: "https://dao54xqhg9jfa.cloudfront.net/OMS-Category/3a3d173d-5537-dafc-0be4-dec0791dcd24/original/MUT.png",
+    title: "Mutton",
+  },
+  {
+    img: "https://dao54xqhg9jfa.cloudfront.net/OMS-Category/21653c3a-4d6d-da71-2432-6833b88e9629/original/RC.png",
+    title: "Ready to cook",
+  },
+  {
+    img: "https://dao54xqhg9jfa.cloudfront.net/OMS-Category/f4053965-f199-80a0-2551-d85d712574e2/original/Prawn_(2).png",
+    title: "Prawns",
+  },
+  {
+    img: "https://dao54xqhg9jfa.cloudfront.net/OMS-Category/49a8dd0c-7254-0b89-b348-b57281c76f5a/original/Coldcuts_(2).png",
+    title: "Cold Cuts",
+  },
+  {
+    img: "https://dao54xqhg9jfa.cloudfront.net/OMS-Category/d9a97969-ebd7-977c-e676-b343a18d7318/original/SPD.png",
+    title: "Spreads",
+  },
+  {
+    img: "https://dao54xqhg9jfa.cloudfront.net/OMS-Category/1bd08fae-c971-390a-ce8a-6f6502f5bd0d/original/Eggs_(1).png",
+    title: "Eggs",
+  },
+  {
+    img: "https://dao54xqhg9jfa.cloudfront.net/OMS-Category/0b7ccd0f-0811-c38b-5420-0317c8006bda/original/Biryani_(2).png",
+    title: "Biryani & Kebab",
+  },
+  {
+    img: "https://dao54xqhg9jfa.cloudfront.net/OMS-Category/66e49926-d949-dfb3-2e79-8052d07f0a3b/original/PBM_6_(8).png",
+    title: "Plant Based Meat",
+  },
+  {
+    img: "https://dao54xqhg9jfa.cloudfront.net/OMS-Category/3f37d093-81cf-3c66-115a-2a4575420d68/original/Masala_1200x840_web.png",
+    title: "Meat Masala",
+  },
+];
 const Navbar = () => {
   const [loc, setLoc] = useState("");
   const success = async (position) => {
@@ -104,34 +152,7 @@ const Navbar = () => {
                   <MenuButton>
                     <Text fontSize="xs">Categories</Text>
                   </MenuButton>
-                  <MenuList width="500px">
-                    <List>
-                      <ListItem>
-                        <MenuItem minH="48px">
-                          <Image
-                            boxSize="2rem"
-                            borderRadius="full"
-                            src="https://placekitten.com/100/100"
-                            alt="Fluffybuns the destroyer"
-                            mr="12px"
-                          />
-                          <span>Fluffybuns the Destroyer</span>
-                        </MenuItem>
-                      </ListItem>
-                      <ListItem>
-                        <MenuItem minH="40px">
-                          <Image
-                            boxSize="2rem"
-                            borderRadius="full"
-                            src="https://placekitten.com/120/120"
-                            alt="Simon the pensive"
-                            mr="12px"
-                          />
-                          <span>Simon the pensive</span>
-                        </MenuItem>
-                      </ListItem>
-                    </List>
-                  </MenuList>
+                  <Category cat={cat} />
                 </Menu>
               </Box>
               <Spacer />
